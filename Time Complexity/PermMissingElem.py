57510dae962f00616fa52ba95998e80e647e33e3
ZhuXW <gy1zhuxiwen@gmail.com>
2015-07-08 11:45:13 +0800

    Add some comment

--- Time Complexity/PermMissingElem.py
+++ Time Complexity/PermMissingElem.py
@@ -1,8 +1,15 @@
-#first time submit C20% P80%
-#def solution(A):
-#    return (set(range(1,len(A)+1)) - set(A)).pop()
-#error£ºThe length of the range calculated error.
+#First time submit C20% P80%
+def solution(A):
+    return (set(range(1,len(A)+1)) - set(A)).pop()
+#Error£ºThe length of the range calculated error.
 
-#second time submit C100% P100%
+#Second time submit C100% P100%
 def solution(A):
     return (set(range(1,len(A)+2)) - set(A)).pop()
+    
+#Third time submit C100% P100%
+#Another solution, but the "sum" method cost more time than the "set"
+def solution(A):
+    N = len(A)
+    total = (N+1)*(N+2)/2
+    return total - sum(A)
--- Time Complexity/TapeEquilibrium.py
+++ Time Complexity/TapeEquilibrium.py
@@ -1,28 +1,28 @@
-#first time submit C66% P100%
-#def solution(A):
-#    lsum = 0
-#    rsum = sum(A)
-#    minimal = abs(lsum-rsum)
-#    for i in A:
-#        lsum += i
-#        rsum -= i
-#        minimal = min(minimal,abs(lsum-rsum))
-#    return minimal
-#error£ºThe position of ¡®i¡¯ should equal len(A)-1, for that the bottom half is not empty.
+#First time submit C66% P100%
+def solution(A):
+    lsum = 0
+    rsum = sum(A)
+    minimal = abs(lsum-rsum)
+    for i in A:
+        lsum += i
+        rsum -= i
+        minimal = min(minimal,abs(lsum-rsum))
+    return minimal
+#Error£ºThe position of ¡®i¡¯ should equal len(A)-1, for that the bottom half is not empty.
 
-#second time submit C16% P33%
-#def solution(A):
-#    lsum = 0
-#    rsum = sum(A)
-#    minimal = abs(lsum-rsum)
-#    for i in range(1,len(A)-1):
-#        lsum += A[i]
-#        rsum -= A[i]
-#        minimal = min(minimal,abs(lsum-rsum))
-#    return minimal
-#error£ºThe value of 'minimal' should not initial with abs(sum(A)),this value maybe less than the value calculate next.
+#Second time submit C16% P33%
+def solution(A):
+    lsum = 0
+    rsum = sum(A)
+    minimal = abs(lsum-rsum)
+    for i in range(1,len(A)-1):
+        lsum += A[i]
+        rsum -= A[i]
+        minimal = min(minimal,abs(lsum-rsum))
+    return minimal
+#Error£ºThe value of 'minimal' should not initial with abs(sum(A)),this value maybe less than the value calculate next.
 
-#third time submit C100% P100%
+#Third time submit C100% P100%
 def solution(A):
     lsum = A[0]
     rsum = sum(A)-A[0]
